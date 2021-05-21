64728aadb064797343db1e808f45d252fb805e17
Damien <damiencampagnapro@gmail.com>
2021-05-21 13:44:37 +0200

    Add of Swagger2 for the documentation of the REST API

--- src/main/java/com/abernathyclinic/mediscreen/service_nosql/ServiceNoSqlApplication.java
+++ src/main/java/com/abernathyclinic/mediscreen/service_nosql/ServiceNoSqlApplication.java
@@ -2,12 +2,31 @@ package com.abernathyclinic.mediscreen.service_nosql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class ServiceNoSqlApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceNoSqlApplication.class, args);
 	}
 
+	/**
+	 * Swagger configuration. <br>
+	 * Ask to work for the controller package only, that's to disable the
+	 * documentation of the default Spring Boot error controller. <br>
+	 */
+	@Bean
+	public Docket swaggerConfigurationBean() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.abernathyclinic.mediscreen.service_nosql.controller"))
+				.paths(PathSelectors.any()).build();
+	}
 }
